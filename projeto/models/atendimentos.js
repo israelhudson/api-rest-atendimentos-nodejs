5ed7b6b5ec56957bdcddb224e7dca24d3e977a4a
Israel Hudson <israelhudson@gmail.com>
2020-07-22 22:17:51 -0300

    validações de erros

--- projeto/models/atendimentos.js
+++ projeto/models/atendimentos.js
@@ -1,15 +1,44 @@
+const moment = require('moment')
 const conexo = require('../infraestrutura/conexao')
 
 class Atendimento {
-    adiciona(atendimento){
-        const sql = 'INSERT INTO Atendimentos SET ?'
+    adiciona(atendimento, res){
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
 
-        conexo.query(sql, atendimento, (erro) => {
-            if(erro)
-                console.log(erro)
-            else
-                console.log(resultados)
-        })
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+            }
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros){
+            res.status(400).json(erros)
+        }else{
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+            conexo.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro)
+                    res.status(400).json(erro)
+                else
+                    res.status(201).json(resultados)
+            })
+        }
     }
 }
 
